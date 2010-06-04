da2df27e5636d42c7b2f08cc7ffb6d008e06fec3
Qing He <qing.x.he@gmail.com>
2010-06-04 12:11:20 +0800

    rework emacs local variables in the file

--- src/include/lexer.hh
+++ src/include/lexer.hh
@@ -1,5 +1,8 @@
-// Copyright (C) 2010 David Lee <live4thee@gmail.com>,
-//                    Qing He <qing.x.he@gmail.com>
+// -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+//
+// Copyright (C)
+//         2010 David Lee <live4thee@gmail.com>
+//         2010 Qing He <qing.x.he@gmail.com>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
