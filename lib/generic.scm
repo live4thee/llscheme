16d6d92cdcc20408b2d7634e607e5f7bacb58dd6
David Lee <live4thee@gmail.com>
2010-07-13 22:02:24 +0800

    Move generic.scm to lib.
    We can implement some library functions here according to SRFI.

--- lib/generic.scm
+++ lib/generic.scm
@@ -1,49 +1,48 @@
 ;; A simple map which accepts only one list
+;; (display (map (lambda (x) (* x x)) '(1 2 3 4)))
 (define (map f lst)
   (if (null? lst)
       '()
       (cons (f (car lst)) (map f (cdr lst)))))
 
-(display (map (lambda (x) (* x x)) '(1 2 3 4)))
 
 ;; Left-fold
-(define (foldl f acc lst)
+;; (display (fold + 0 '(1 2 3 4)))
+(define (fold f acc lst)
   (if (null? lst)
       acc
-      (foldl f (f acc (car lst)) (cdr lst))))
+      (fold f (f acc (car lst)) (cdr lst))))
 
-(display (foldl + 0 '(1 2 3 4)))
 
 ;; Right-fold
-(define (foldr f acc lst)
+;; (display (fold-right + 0 '(1 2 3 4)))
+(define (fold-right f acc lst)
   (if (null? lst)
       acc
-      (f acc (foldr f (car lst) (cdr lst)))))
+      (f acc (fold-right f (car lst) (cdr lst)))))
 
-(display (foldr + 0 '(1 2 3 4)))
 
 ;; for-each
+;; (for-each (lambda (x) (display (+ x 1))) '(1 2 3 4))
 (define (for-each f lst)
   (if (null? lst)
       '()
       (begin (f (car lst)) (for-each f (cdr lst)))))
 
-(for-each (lambda (x) (display (+ x 1))) '(1 2 3 4))
 
 ;; append two lists
+;; (display (append '(1 2) '(3 4)))
 (define (append l1 l2)
   (if (null? l1)
       l2
       (cons (car l1) (append (cdr l1) l2))))
 
-(display (append '(1 2) '(3 4)))
 
 ;; filter
+;; (display (filter (lambda (x) (> x 2)) '(1 2 3 4)))
 (define (filter p lst)
   (if (null? lst)
       '()
       (if (p (car lst))
 	  (cons (car lst) (filter p (cdr lst)))
 	  (filter p (cdr lst)))))
-
-(display (filter (lambda (x) (> x 2)) '(1 2 3 4)))
